ec5805b0dfedbcc0650950e0049f8f75819d981b
yxj <1639884717@qq.com>
2020-05-12 20:32:34 +0800

    第一次修改克隆的git-test工程

--- git-test/src/com/itcast/git/FirstGit.java
+++ git-test/src/com/itcast/git/FirstGit.java
@@ -5,9 +5,11 @@ public class FirstGit {
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
+        System.out.println("hello git clone");
     }
     public void test() {
         int i = 0;
         i+=10;
+        System.out.println(i);
     }
 }
