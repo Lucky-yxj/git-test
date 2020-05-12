30718170052dd5452c4b12a1cc15e56819d23206
yxj <1639884717@qq.com>
2020-05-12 20:45:05 +0800

    修改FirstGit

--- git-test/src/com/itcast/git/FirstGit.java
+++ git-test/src/com/itcast/git/FirstGit.java
@@ -2,6 +2,7 @@ package com.itcast.git;
 
 public class FirstGit {
     public static void main(String[] args) {
+        System.out.println("在菜单中选择“vcs”→Import into Version Control→Create Git Repository然后选择本项目的父目录，就会在其中添加.git文件，然后就可以操作了");
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
